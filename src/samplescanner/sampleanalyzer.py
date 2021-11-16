121ad9f95750541c5ab75906b16c2e08a2ac5afb
TriYop <dev@yvanjanet.net>
2021-11-17 00:20:20 +0100

    Fixed key parsing

--- src/samplescanner/sampleanalyzer.py
+++ src/samplescanner/sampleanalyzer.py
@@ -1,3 +1,6 @@
+import re
+import logging
+
 class SampleAnalyzer():
 
     KEYS = {
@@ -15,12 +18,23 @@ class SampleAnalyzer():
         "B": 11,
     }
 
+    logger = logging.getLogger("SampleAnalyzer")
+
     def __init__(self, filename:str):
         self.sample_filename = filename
 
     def _get_sample_key(self) -> int:
-        # split sample name to determin sample pitch
-        pass
+        m = re.search(r'_([A-G][#b]?)(-?[0-9])[_\.]', self.sample_filename)
+        pitch = 60
+        if m is not None:
+
+            keydef= m.group(1)
+            self.logger.info(keydef)
+            self.logger.info(m.group(2))
+            octave= int(m.group(2))
+            pitch= self.KEYS.get(keydef, 0) + 12*(octave+1)
+
+        return pitch
 
     def analyze(self):
         return {
